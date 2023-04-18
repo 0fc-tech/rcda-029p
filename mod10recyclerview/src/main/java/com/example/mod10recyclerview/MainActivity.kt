d99230b8853c70a9a23a0b60f7782bcfcac8faf6
riderofzion <quentinrouet@hotmail.fr>
2023-04-18 16:40:09 +0200

    mod10recyclerview tuning list with click management on item list

--- mod10recyclerview/src/main/java/com/example/mod10recyclerview/MainActivity.kt
+++ mod10recyclerview/src/main/java/com/example/mod10recyclerview/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mod10recyclerview.bo.Tuning
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,14 +13,48 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val recyclerViewTuning : RecyclerView= findViewById(R.id.recyclerViewTuning)
         recyclerViewTuning.layoutManager = LinearLayoutManager(this)
-        recyclerViewTuning.adapter = TuningAdapter(
-            arrayListOf(
+        recyclerViewTuning.adapter = TuningAdapter(arrayListOf(
                 Tuning("Renault 21", "grise", 21, 5, "Christophe", 13.5f, 90.0f, "https://img.over-blog-kiwi.com/0/93/23/39/20170430/ob_afccee_18209113-1524099324298404-652453166579.jpg"),
-                Tuning("Nissan Skyline GT-R R34", "bleue", 65, 2, "Brian O'Conner", 18.5f, 330.0f, "https://cdn.shopify.com/s/files/1/0888/2222/products/R34-1_1200x.jpg?v=1571720539"),
-                Tuning("Toyota Supra", "blanche", 70, 2, "Dominic Toretto", 17.0f, 280.0f, "https://www.carscoops.com/wp-content/uploads/2021/01/Toyota-Supra-MKIV-Tuned-1-1024x555.jpg"),
-                Tuning("Mitsubishi Lancer Evolution", "bleue", 20, 5, "Mia Toretto", 14.5f, 330.0f, "https://cdn.bringatrailer.com/wp-content/uploads/2019/02/2006_mitsubishi_lancer_evo_ix_1551857556b7a6a-2.jpg"),
-                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://i.pinimg.com/originals/63/63/74/63637425a3f3ab3e8f7749da0ee59f72.jpg")
-            )
-        )
+                Tuning("Nissan Skyline GT-R R34", "bleue", 65, 2, "Brian O'Conner", 18.5f, 330.0f, "https://delessencedansmesveines.com/wp-content/uploads/2022/08/z-DLEDMV-2022-Nissan-Skyline-R34-GTR-VSpecII-Motorex-Paul-Walker-001-1080x675.jpg"),
+                Tuning("Toyota Supra", "blanche", 70, 2, "Dominic Toretto", 17.0f, 280.0f, "https://www.tuningblog.eu/wp-content/uploads/2022/07/2023-Toyota-Supra-Edition-mattweiss-1.jpg"),
+                Tuning("Mitsubishi Lancer Evolution", "bleue", 20, 5, "Mia Toretto", 14.5f, 330.0f, "https://www.super-hobby.fr/zdjecia/5/0/8/19031_rd.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg"),
+                Tuning("Honda Civic", "rouge", 35, 4, "Jesse", 12.0f, 180.0f, "https://picolio.auto123.com/auto123-media/articles/2018/8/65042/Honda-Civic-Type-R-2018-VA%20(19)fr.jpg")
+            )){tuningClicked->
+            Snackbar.make(
+                findViewById(R.id.recyclerViewTuning),
+                tuningClicked.baseCar,
+                Snackbar.LENGTH_LONG).show()
+        }
     }
 }
--- mod10recyclerview/src/main/java/com/example/mod10recyclerview/TuningAdapter.kt
+++ mod10recyclerview/src/main/java/com/example/mod10recyclerview/TuningAdapter.kt
@@ -1,13 +1,19 @@
 package com.example.mod10recyclerview
 
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.mod10recyclerview.bo.Tuning
 import com.example.mod10recyclerview.databinding.TemplateTuningBinding
+import com.squareup.picasso.MemoryPolicy
+import com.squareup.picasso.Picasso
 
-class TuningAdapter(val arrayList: ArrayList<Tuning>) : RecyclerView.Adapter<TuningAdapter.TuningVH>() {
+class TuningAdapter(
+    val arrayList: ArrayList<Tuning>,
+    val onClickTuning : (tuning : Tuning) -> Unit)
+    : RecyclerView.Adapter<TuningAdapter.TuningVH>() {
     class TuningVH(val binding: TemplateTuningBinding) : ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TuningVH {
@@ -18,7 +24,14 @@ class TuningAdapter(val arrayList: ArrayList<Tuning>) : RecyclerView.Adapter<Tun
     }
 
     override fun onBindViewHolder(holder: TuningVH, position: Int) {
-        holder.binding.tuning = arrayList[position]
+        val tuning = arrayList[position]
+        holder.itemView.setOnClickListener {
+            onClickTuning(tuning)
+        }
+        holder.binding.tuning = tuning
+        Picasso.get()
+            .load(Uri.parse(tuning.imageUrl)).memoryPolicy(MemoryPolicy.NO_STORE,MemoryPolicy.NO_CACHE)
+            .into(holder.binding.imageViewTuning)
     }
 
     override fun getItemCount(): Int = arrayList.size
